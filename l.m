3467b1150eb43028615cc7ee8a47ce920c4d43c3
RocketHcgs <rockethcgs@gmail.com>
2020-06-26 00:07:04 +0800

    step 3

---
+++ l.m
@@ -0,0 +1,9 @@
+%计算误差方程常数项
+[Ld, Lm, Ls, ad, am, as, bd, bm, bs]=textread('l.txt','%f %f %f %f %f %f %f %f %f');
+Li = dms2degrees([Ld, Lm, Ls]);
+ai = dms2degrees([ad, am, as]);
+bi = dms2degrees([bd, bm, bs]);
+li = -degrees2dms(Li - (ai - bi));
+for i = 1:numel(li)
+    disp([i,li(i,3)]);
+end
--- x0.m
+++ x0.m
@@ -1,7 +1,7 @@
 % 计算C, F, G, H, J的近似坐标值X0
-[xa, ya, xb, yb, a1, a2, a3, b1, b2, b3]=textread('x0.txt','%f %f %f %f %f %f %f %f %f %f');
-a = dms2degrees([a1, a2, a3]);
-b = dms2degrees([b1, b2, b3]);
+[xa, ya, xb, yb, ad, am, as, bd, bm, bs]=textread('x0.txt','%f %f %f %f %f %f %f %f %f %f');
+a = dms2degrees([ad, am, as]);
+b = dms2degrees([bd, bm, bs]);
 xp = (xa.*cotd(b)+xb.*cotd(a)+ya-yb)./(cotd(a)+cotd(b));
 yp = (ya.*cotd(b)+yb.*cotd(a)-xa+xb)./(cotd(a)+cotd(b));
 disp([xp yp]);
