1af865972f2713b55d6e0df074690af96730913d
RocketHcgs <rockethcgs@gmail.com>
2020-06-25 11:06:42 +0800

    step 1 2

---
+++ a.m
@@ -0,0 +1,17 @@
+[xa, ya, xb, yb]=textread('a.txt','%f %f %f %f');
+a0 = atan((yb-ya)./(xb-xa));
+a0 = rad2deg(a0);
+for i = 1:numel(a0)
+    if(a0(i)<0)
+        a0(i)=-a0(i);
+    end
+    if((yb(i)-ya(i))>0 && (xb(i)-xa(i))>0)
+        disp(degrees2dms(a0(i)));
+    elseif((yb(i)-ya(i))>0 && (xb(i)-xa(i))<0)
+        disp(degrees2dms(180-a0(i)));
+    elseif((yb(i)-ya(i))<0 && (xb(i)-xa(i))<0)
+        disp(degrees2dms(180+a0(i)));
+    elseif((yb(i)-ya(i))<0 && (xb(i)-xa(i))>0)
+        disp(degrees2dms(360-a0(i)));
+    end
+end
---
+++ x0.m
@@ -0,0 +1,6 @@
+[xa, ya, xb, yb, a1, a2, a3, b1, b2, b3]=textread('x0.txt','%f %f %f %f %f %f %f %f %f %f');
+a = dms2degrees([a1, a2, a3]);
+b = dms2degrees([b1, b2, b3]);
+xp = (xa.*cotd(b)+xb.*cotd(a)+ya-yb)./(cotd(a)+cotd(b));
+yp = (ya.*cotd(b)+yb.*cotd(a)-xa+xb)./(cotd(a)+cotd(b));
+disp([xp yp]);
